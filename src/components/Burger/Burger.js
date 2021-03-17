178789f2e0176493ec31cf2f938536c10e14ffbe
Vladislav.Yakimovsky <vladislav.yakimovsky@C02DL7A9MD6R.local>
2021-03-18 01:44:01 +0300

    add warning if burgers ingredients not chosen

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,13 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngridient/BurgerIngredient';
 
 const Burger = ({ ingredients }) => {
-  const transformedIngredients = Object.keys(ingredients).map(igKey => {
-    return [...Array(ingredients[igKey])].map((_, i) => (
+  let transformedIngredients = Object.keys(ingredients).map(igKey => (
+    [...Array(ingredients[igKey])].map((_, i) => (
       <BurgerIngredient key={igKey + i} type={igKey} />
     ))
-  });
+  )).reduce((arr, el) => arr.concat(el), []);
+
+  if (!transformedIngredients.length) transformedIngredients = <p>Please start adding ingredients!</p>
 
   return (
     <div className={classes.Burger}>
